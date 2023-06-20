9a73ce4538890541270d7d7d537ce97427a88ad0
Nikita Bolshakov <bnikmik@gmail.com>
2023-06-20 15:25:01 +0300

    Initial commit

---
+++ src/main/java/com/exchangeratebot/ExchangeRateBotApplication.java
@@ -0,0 +1,13 @@
+package com.exchangeratebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exchangeratebot/ExchangeRateBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchangeratebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
